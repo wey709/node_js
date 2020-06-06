f9520f5f6c8a03b2190515d1b37a705cb8e081ca
Eunyoung Won <verte709@gmail.com>
2020-06-07 03:26:00 +0900

    done

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const mustacheExpress = require("mustache-express");
 const bodyParser = require('body-parser');
+
 const { Client } = require('pg');
+
 require('dotenv').config();
-const client = new Client({
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    database: process.env.PG_DATABASE
-});
+
 
 
 const app = express();
@@ -29,22 +25,67 @@ app.get('/list', (req, res)=>{
 
 
 app.get('/twt', (req, res) => {
+  const client = new Client({
+    user: 'young',
+   password: 930709,
+   host: process.env.PG_HOST,
+   port: process.env.PG_PORT,
+   database: process.env.PG_DATABASE
+  });
 
-    const client = new Client();
     client.connect()
       .then(() => {
-        return client.query('SELECT * FROM trump;');
+        console.log('connected')
+        return client.query('SELECT * FROM trump_may;');
       })
       .then((results) => {
-        console.log(results)
+        //console.log(results)
         res.render('list', results);
       })
       .catch((err) => {
-        //console.log('error', err);
+        console.log('error', err);
         res.send('Something bad happened');
       });
   
   });
+
+
+
+  app.get('/twt/rt/:id', (req, res)=>{
+
+    const client = new Client({
+      user: 'young',
+      password: 930709,
+      host: process.env.PG_HOST,
+      port: process.env.PG_PORT,
+      database: process.env.PG_DATABASE
+    });
+
+    client.connect()
+    .then(() => {
+      const sql = "select * from trump_may_rt where permalink=(select permalink from trump_may where id = ($1));";
+      const param_ = [req.params.id];
+      return client.query(sql, param_);
+
+    })
+    .then((results) => {
+      //console.log('result?', result);
+      res.render('list_rt', results);
+    })
+    .catch((err) => {
+      console.log('err', err);
+      res.send("error")
+    });
+
+    
+
+
+})
+
+
+
+
+
 
 app.get('/twt/add', (req, res)=>{
     res.render('twt-form');
@@ -56,10 +97,6 @@ app.post('/twt/add', (req, res)=>{
     client.connect()
     .then(() => console.log('connected successfully'))
 
-    //console.log(req.body['contents'])
-
-    // let text = 'INSERT INTO my_twt(contents, created_on) VALUES($1, $2) RETURNING *'
-    // 
 
     const sql = "INSERT INTO my_twt(contents, created_on) VALUES($1, $2)";
     const values = [req.body['contents'], req.body['created_on']]
